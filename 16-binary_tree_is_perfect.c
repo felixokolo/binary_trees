baa9332e5fd018eb0f6c3ba86dcb6ee031f7a18a
felixokolo <pheelix.info@gmail.com>
2022-08-25 20:36:44 +0100

    correction

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include <math.h>
 
 /**
  * binary_tree_is_perfect - function that checks
@@ -10,21 +11,72 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int is_perfect = 1;
+	int is_perfect = 0;
+	size_t nodes = 0, height = 0;
 
 	if (tree == NULL)
 	return (0);
+	height = binary_tree_height(tree);
+	nodes = binary_tree_nodes(tree);
+	if (nodes == (pow(2, height) - 1))
+		is_perfect = 1;
+	return (is_perfect);
+}
+
+/**
+ * binary_tree_height - function that measures
+ * the height of a binary tree
+ * @tree: pointer to the root node of the tree to
+ * measure the height.
+ * Return: height of tree or 0 if NULL
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height = 0, right = 0, left = 0;
+
+	if (tree == NULL)
+	return (height);
 	if (tree->left != NULL)
 	{
-		if (tree->right == NULL)
-		is_perfect = 0;
-		is_perfect *= binary_tree_is_perfect(tree->left);
+		left++;
+		left += binary_tree_height(tree->left);
 	}
 	if (tree->right != NULL)
 	{
-		if (tree->left == NULL)
-		is_perfect = 0;
-		is_perfect *= binary_tree_is_perfect(tree->right);
+		right++;
+		right += binary_tree_height(tree->right);
 	}
-	return (is_perfect);
+	if (left == 0 && right == 0)
+	return (0);
+	else
+	return (left >= right ? left : right);
+	return (0);
+}
+
+/**
+ * binary_tree_nodes - function that counts the
+ * nodes with at least 1 child in a binary tree.
+ * @tree: pointer to the root node of the tree
+ * to count the number of nodes
+ * Return: number of nodes
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes = 0;
+
+	if (tree == NULL)
+	return (0);
+	if (tree->left != NULL || tree->right != NULL)
+	nodes++;
+	if (tree->left != NULL)
+	{
+		nodes += binary_tree_nodes(tree->left);
+	}
+	if (tree->right != NULL)
+	{
+		nodes += binary_tree_nodes(tree->right);
+	}
+	return (nodes);
 }
